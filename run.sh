c9ede0fe3cc4ded17da22b81ca5f14c20d9723e6
Mitar <mitar.github@tnode.com>
2016-09-12 20:00:33 -0700

    Restart container automatically.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #/bin/bash -e
 
-docker run -d -p 53:53/tcp -p 53:53/udp --name dns -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-slave
+mkdir -p /var/log/powerdns
+
+docker run -d --restart=always -p 53:53/tcp -p 53:53/udp --name dns -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-slave
