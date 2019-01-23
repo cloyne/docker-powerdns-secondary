265924eb7c4b269715d303f1a571424384814841
Eleanor Cawthon <cawthon.e@gmail.com>
2019-01-22 18:35:21 -0800

    use emancipated docker image

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /var/log/powerdns
 
-docker run -d --restart=always -p 53:53/tcp -p 53:53/udp --name dns -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-slave
+docker run -d --restart=always -p 53:53/tcp -p 53:53/udp --name dns -v /srv/var/log/powerdns:/var/log/powerdns cloyne/powerdns-secondary
